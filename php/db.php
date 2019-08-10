4c3cf7ad04e3cf06d404424101548c1717687012
Ding Lin <dinglin@dings-mbp.home>
2019-08-10 15:36:05 -0400

    emmm

--- php/db.php
+++ php/db.php
@@ -2,7 +2,7 @@
 ob_start();
 
 try {
-    $con = new PDO("mysql:dbname=xd;host=149.4.211.180", "lidi5680", "23135680");
+    $con = new PDO("mysql:host=149.4.211.180;dbname=lidi5680", "lidi5680", "23135680");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch (PDOExeption $e) {
     echo "Error: " . $e->getMessage();
